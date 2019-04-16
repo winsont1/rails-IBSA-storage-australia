d88ec9662f4451518b935161860971b787a777fd
Winson Tan <winsont89@gmail.com>
2019-04-16 16:16:30 +1000

    Add controller migration files

---
+++ app/controllers/consignments_controller.rb
@@ -0,0 +1,2 @@
+class ConsignmentsController < ApplicationController
+end
---
+++ test/controllers/consignments_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ConsignmentsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
