da10448effb8d19d4f2d4cab2a51215007a09525
Winson Tan <winsont89@gmail.com>
2019-04-16 16:17:28 +1000

    Docs: Add faker gem and seeds to app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 10 fake goods...'
+10.times do
+  consignment = Consignment.new(
+    source: Faker::Address.country,
+    destination: Faker::Address.state,
+    entry_time: Faker::Time.between(DateTime.now - 365,
+      DateTime.now - 182),
+    exit_time: nil,
+    given_consignment_ID: 'A'+rand(1000..3000).to_s)
+  consignment.save!
+
+  good = Good.new(
+    name: Faker::Construction.material,
+    good_type: Faker::Construction.subcontract_category,
+    consignment: consignment
+  )
+  good.save!
+
+end
+puts 'Finished!'
