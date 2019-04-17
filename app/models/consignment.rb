4ff489b607319c233c0447ff96e8672f235932a4
Winson Tan <winsont89@gmail.com>
2019-04-17 10:03:10 +1000

    Fixed bug in updating shipped goods

--- app/models/consignment.rb
+++ app/models/consignment.rb
@@ -6,8 +6,9 @@ class Consignment < ApplicationRecord
     CSV.foreach(file.path, headers: true) do |row|
       # Check if Consignment ID is in system and Exit_time is true
       if (consignment_out = Consignment.find_by(given_consignment_ID: row[1])) && (row[7].nil? == false) # TODO: Raise error if consignment already in and exit time is nil
-        consignment_out.destroy
-        # consignment_out.exit_time = Faker::Time.between(DateTime.now - 181, DateTime.now) # TODO: Having issue with updating time
+        consignment_out.exit_time = row[7]
+        consignment_out.status = 'Shipped'
+        consignment_out.save!
       else
         consignment = Consignment.new(
           source: row[4], # TODO: Change to hash
